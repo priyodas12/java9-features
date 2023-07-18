d14dbb02864e4aa13d1c904967e9d0372b29c5ac
Priyobrato.Das <priyodas12@gmail.com>
2023-07-18 23:25:45 +0530

    compact string in java 9

---
+++ src/main/java/io/priyotech/CompactStrings.java
@@ -0,0 +1,10 @@
+package io.priyotech;
+
+public class CompactStrings {
+    public static void main(String[] args) {
+        /**String underline data structure will be byte array*/
+        String a="abc";
+        int length = a.getBytes().length;
+        System.out.println(length);
+    }
+}
