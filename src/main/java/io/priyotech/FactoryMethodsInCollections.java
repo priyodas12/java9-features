4863e9c0e97fc2879ed82158f076c200e17d163e
Priyobrato.Das <priyodas12@gmail.com>
2023-07-18 22:42:21 +0530

    adding static factory method for list and collections

--- src/main/java/io/priyotech/FactoryMethodsInCollections.java
+++ src/main/java/io/priyotech/FactoryMethodsInCollections.java
@@ -1,2 +1,53 @@
-package io.priyotech;public class FactoryMethodsInCollections {
+package io.priyotech;
+
+import java.util.*;
+
+public class FactoryMethodsInCollections {
+
+    public static void unModifiableListWithAdd(){
+        List<String> strList=new ArrayList<>();
+        strList.add("abc");
+        strList.add("bca");
+        strList.add("cab");
+
+        List<String> stringList= Collections.unmodifiableList(strList);
+        //stringList.add("dab"); //immutable list
+        System.out.printf(String.format("unmodifiable list is %s \n",stringList));
+
+    }
+
+    public static void unModifiableListAsList(){
+        List<String> strList=Collections.unmodifiableList(Arrays.asList("abc","bca","cab"));
+        //strList.add("dab"); //immutable list
+        System.out.printf(String.format("unmodifiable list is %s \n",strList));
+
+    }
+
+    public static void main(String[] args) {
+
+        unModifiableListWithAdd();
+
+        unModifiableListAsList();
+        //List.of()->static factory method.
+        List<String> immutableList = List.of("dav", "ogyh", "uan");
+        List<String> immutableList2 = List.of("g", "a", "z", "u", "q", "i", "l", "b", "j", "c");
+        List<String> immutableList3 = List.of("g", "a", "z", "u", "q", "i", "l", "b", "j", "c", "d");
+        Set<String> immutableSet = Set.of("a", "k", "z", "u", "v", "i", "w", "b", "j", "c");
+        Map<String, Integer> immutableMap =
+                Map.ofEntries(
+                        Map.entry("a", 100),
+                        Map.entry("b", 200),
+                        Map.entry("c", 300)
+                );
+        try {
+            immutableList.add("igs");
+            immutableList3.add("x");
+        }catch (UnsupportedOperationException e){
+            System.out.println("Cannot add element due to:  "+e);
+        }
+        System.out.println(immutableList);
+        System.out.println(immutableList3);
+        System.out.println(immutableSet);
+        System.out.println(immutableMap);
+    }
 }
