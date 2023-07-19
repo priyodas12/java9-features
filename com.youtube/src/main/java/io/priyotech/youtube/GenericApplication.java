22a3a34b3369e2a8958eedcc6469e0e63a87c209
Priyobrato.Das <priyodas12@gmail.com>
2023-07-19 19:53:28 +0530

    introduced module.jshell

--- com.youtube/src/main/java/io/priyotech/youtube/GenericApplication.java
+++ com.youtube/src/main/java/io/priyotech/youtube/GenericApplication.java
@@ -1,7 +1,11 @@
 package io.priyotech.youtube;
 
+import java.util.logging.Logger;
+
 public class GenericApplication {
+    Logger logger=Logger.getLogger(GenericApplication.class.getName());
     public void HelloYoutube(){
+
         System.out.println(System.currentTimeMillis()+" : Hello Youtube!");
     }
 }
--- com.youtube/src/module-info.java
+++ com.youtube/src/module-info.java
@@ -1,3 +1,4 @@
 module com.youtube{
+    requires java.logging;
     export io.priyotech.youtube;
 }
