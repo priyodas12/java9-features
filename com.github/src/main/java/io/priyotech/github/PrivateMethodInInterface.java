2f97d6d1ff677d78f507221b3bf8bb0c3ae7bc3a
Priyobrato.Das <priyodas12@gmail.com>
2023-07-19 20:37:14 +0530

    private method in interface

---
+++ com.github/src/main/java/io/priyotech/github/PrivateMethodInInterface.java
@@ -0,0 +1,31 @@
+package io.priyotech.github;
+
+public interface PrivateMethodInInterface {
+
+    default void checkConditionOne(Long n){
+        commonDataCheck(n);
+        System.out.println("method>>>checkConditionOne starts");
+        System.out.println(n<200L?"Valid Value":"Invalid Value!");
+        System.out.println("method>>>checkConditionOne completes\n");
+    }
+
+    default void checkConditionTwo(Long n){
+        commonDataCheck(n);
+        System.out.println("method>>>checkConditionTwo starts");
+        System.out.println(n>100L?"Valid Value":"Invalid Value!");
+        System.out.println("method>>>checkConditionTwo completes\n");
+    }
+
+    private void commonDataCheck(Long n){
+        System.out.println("method>>>commonDataCheck");
+        System.out.println(n != null && n >0L ?"Valid Long Value":"invalid Long value");
+    }
+}
+
+class PrivateMethodInInterfaceImpl implements PrivateMethodInInterface{
+    public static void main(String[] args) {
+        PrivateMethodInInterfaceImpl instance=new PrivateMethodInInterfaceImpl();
+        instance.checkConditionOne(120L);
+        instance.checkConditionTwo(220L);
+    }
+}
