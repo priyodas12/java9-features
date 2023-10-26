d9eeeea3e9b9916822602411ade764d4a801ea87
Priyobrato.Das <priyodas12@gmail.com>
2023-10-26 07:56:32 +0530

    immutable map

--- com.github/src/main/java/io/priyotech/github/DifferenceInStaticMethods.java
+++ com.github/src/main/java/io/priyotech/github/DifferenceInStaticMethods.java
@@ -1,5 +1,7 @@
 package io.priyotech.github;
 
+import com.google.common.collect.ImmutableMap;
+
 import java.sql.Array;
 import java.util.*;
 
@@ -22,17 +24,32 @@ public class DifferenceInStaticMethods {
         /**Java-8 in Set*/
         Set<String> strSet=new HashSet<>();
         strSet.addAll(Arrays.asList("12","13","14","12"));
-        System.out.printf("strSet : %s ,",strSet);
+        System.out.printf("\nstrSet : %s ,",strSet);
         Set<String> unmodifiableSet=Collections.unmodifiableSet(strSet);
-        System.out.printf("unmodifiableSet : %s",unmodifiableSet);
+        System.out.printf("\nunmodifiableSet : %s ,",unmodifiableSet);
 
         /**Java-9 in Set*/
         //null not allowed,NullPointerException: Cannot invoke "Object.hashCode()" because "pe" is null
         Set<String> unmodifiableSet2=Set.of("12","13","14","18");
-        System.out.printf("unmodifiableSet2 : %s",unmodifiableSet2);
+        System.out.printf("unmodifiableSet2 : %s ,",unmodifiableSet2);
 
-        Set<String> unmodifiableSet3=Set.of("12","13","14","12");//IllegalArgumentException: duplicate element: 12
-        System.out.printf("unmodifiableSet3 : %s",unmodifiableSet3);
+        //Set<String> unmodifiableSet3=Set.of("12","13","14","12");//IllegalArgumentException: duplicate element: 12
+        //System.out.printf("unmodifiableSet3 : %s",unmodifiableSet3);
+
+        /**Java-8 in Map*/
+        Map<String,Integer> playerMap=new HashMap<>();
+        playerMap.put("virat",10);
+        playerMap.put("rohit",45);
+        playerMap.put("dhoni",7);
+        playerMap.put("rohit",42);
+        System.out.printf("\nplayerMap : %s ,",playerMap);
+        Map<String,Integer> immutableMap= ImmutableMap.copyOf(playerMap);
+        System.out.printf("\nimmutableMap : %s ,",immutableMap);
+
+        /**Java-9 in Map*/
+        //duplicate keys are not allowed
+        Map<String,Integer> immutableMap2= Map.of("b",20,"c",101,"a",12);
+        System.out.printf("\nimmutableMap2 : %s ,",immutableMap2);
 
     }
 }
