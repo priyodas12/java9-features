deb870faeb7d89542e5157c5817d6bf5a03d5358
Priyobrato.Das <priyodas12@gmail.com>
2023-10-31 16:13:34 +0530

    stream with iterate 3 args

--- com.github/src/main/java/io/priyotech/github/StreamIterateWithPredicate.java
+++ com.github/src/main/java/io/priyotech/github/StreamIterateWithPredicate.java
@@ -1,12 +1,26 @@
 package io.priyotech.github;
 
+import java.time.LocalDate;
 import java.util.stream.Stream;
 
 public class StreamIterateWithPredicate {
 
     public static void main(String[] args) {
-        Stream.iterate(1,n->n*2)
+        Stream.iterate(0,n->n+2)
                 .limit(20)
+                .forEach(System.out::println);
+
+        Stream.iterate(1,n->n*2)
+                .takeWhile(n->n<12)
+                .forEach(System.out::println);
+
+        Stream.iterate(1, n->n<20, n->n*2)
+                .forEach(System.out::println);
+
+        LocalDate today=LocalDate.now();
+
+        Stream.iterate(today,date->date.plusDays(1))
+                .limit(7)
                 .forEach(System.out::println);
     }
 }
